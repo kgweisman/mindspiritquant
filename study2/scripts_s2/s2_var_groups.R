93cc8b661c88d39cefebea2fced9580139e23e8f
Anthro User <anthrouser@ant-fvfwl34xhv2h.local>
2019-11-18 10:38:59 -0500

    add studies 2-4

---
+++ study2/scripts_s2/s2_var_groups.R
@@ -0,0 +1,30 @@
+# GROUPS OF VARIABLES RELEVANT TO STUDY 2
+
+require(tidyverse)
+
+# demographics
+# s2_var_demo <- c("date", "researcher", "country", "site", "religion", 
+#                  "subject_id", "subject_gender", "subject_age", "subject_job", 
+#                  "subject_schedule", "subject_livedhere", "subject_lang", 
+#                  "subject_marital", "subject_hs", "subject_liveswith", 
+#                  "servicesperweek", "specialrole")
+
+# absorption
+s2_var_abs <- c("exwl_01", "exwl_02", "exwl_03", "exwl_04", "exwl_05", "exwl_06",
+                "exwl_07", "exwl_08", "exwl_09", "exwl_10", "exwl_11", "exwl_12",
+                "exwl_13", "exwl_14", "exwl_15", "exwl_16", "exwl_17", "exwl_18",
+                "exwl_19", "exwl_20", "exwl_21", "exwl_22", "exwl_23", "exwl_24",
+                "exwl_25", "exwl_26", "exwl_27", "exwl_28", "exwl_29", "exwl_30",
+                "exwl_31", "exwl_32", "exwl_33", "exwl_34")
+
+# daily spiritual experiences
+s2_var_dse <- c("dse_01", "dse_02", "dse_03", "dse_04", "dse_05", "dse_06", 
+                "dse_07", "dse_08", "dse_09", "dse_10", "dse_11", "dse_12",
+                "dse_13", "dse_14") # omitted dse_15 and dse_16 (not experiences)
+
+# spiritual events
+s2_var_spev <- c("spev_01", "spev_02", "spev_03", "spev_04", "spev_05", "spev_06",
+                 "spev_07", "spev_08", "spev_09", "spev_10", "spev_11", "spev_12",
+                 "spev_13", "spev_14", "spev_15", "spev_16", "spev_17", "spev_18",
+                 "spev_19", "spev_20", "spev_21", "spev_22")
+
---
+++ study3/scripts_s3/s3_var_groups.R
@@ -0,0 +1,36 @@
+# GROUPS OF VARIABLES RELEVANT TO STUDY 3
+
+require(tidyverse)
+
+# porosity
+s3_var_por <- c("epi_1_01", "epi_1_02", "epi_1_03", "epi_1_04", "epi_1_05", 
+                "epi_1_06", "epi_1_07", "epi_1_08", "epi_1_09", "epi_1_10", 
+                "epi_1_11", "epi_1_12", "epi_1_13", "epi_1_14", "epi_1_15", 
+                "epi_1_16")
+
+# spiritual events
+s3_var_spex <- c("epi_2_02", "epi_2_03", "epi_2_04", "epi_2_05",
+                 "epi_2_06", "epi_2_07", "epi_2_08", "epi_2_09", "epi_2_10",
+                 "epi_2_16", "epi_2_17", "epi_2_18", "epi_2_19", "epi_2_21")
+
+s3_var_being_us <- c("epi_2_usa11", "epi_2_usa12", "epi_2_usa13", 
+                     "epi_2_usa14", "epi_2_usa15") 
+s3_var_being_gh <- c("epi_2_gha11", "epi_2_gha12", "epi_2_gha13", 
+                     "epi_2_gha14", "epi_2_gha15") 
+s3_var_being_th <- c("epi_2_thl11", "epi_2_thl12a", "epi_2_thl12b", 
+                     "epi_2_thl12c", "epi_2_thl12d", "epi_2_thl12e", 
+                     "epi_2_thl12f", "epi_2_thl12g", "epi_2_thl12h", 
+                     "epi_2_thl12i", "epi_2_thl12j", "epi_2_thl12k")
+s3_var_being_ch <- c("epi_2_chn11", "epi_2_chn12", "epi_2_chn13", 
+                     "epi_2_chn14", "epi_2_chn15")
+s3_var_being_vt <- c("epi_2_vut11", "epi_2_vut12", "epi_2_vut13", 
+                     "epi_2_vut14", "epi_2_vut15")
+
+s3_var_being <- c(s3_var_being_us, s3_var_being_gh, s3_var_being_th, 
+                  s3_var_being_ch, s3_var_being_vt)
+
+s3_var_spirit <- c(s3_var_spex, s3_var_being)
+
+# other extraordinary events
+s3_var_other <- c("epi_2_20", "epi_2_22", "epi_2_23")
+
